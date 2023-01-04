79024437f70c7e770105ac388bee2540a7612cff
Punit Agarwala <punitagarwalakalna@gmail.com>
2023-01-04 23:54:19 +0530

    first commit for cold project

---
+++ cold.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(void){
+    bool m;
+    cout<<"is it so cold?"<<endl;
+    cin>>m;
+    if(m==true)
+        cout<<"yes"<<endl;
+    else
+        cout<<"no"<<endl;
+    return 0;
+    
+}
